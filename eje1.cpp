481c47da6a3662ef6d1d9c13fc73a8b5b20883a4
amht-a <hernandez.trejo.a.monserrat@gmail.com>
2023-02-16 23:56:44 -0600

    Create eje1.cpp

---
+++ eje1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float lado1=0.0f,lado2=0.0f, area=0.0f,perimetro=0.0f;
+    cout<<"Ingrese la base del rectangulo "<<endl;
+    cin>>lado1;
+    cout<<"Ingrese la altura rectangulo "<<endl;
+    cin>>lado2;
+    area=lado1*lado2;
+    perimetro=2*lado1+2*lado2;
+    cout<<"El area del rectangulo es :"<<area<<endl;
+    cout<<"El perimetro del rectangulo es :"<<perimetro<<endl;
+    return 0;
+}
